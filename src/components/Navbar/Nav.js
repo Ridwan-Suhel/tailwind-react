e996eb48012d4be348103d284d5717a49786cd91
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-03-30 20:19:48 +0600

    navbar collapse added

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "./Link/Link";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
 
 const Nav = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, name: "Home", link: "/home" },
     { id: 2, name: "About", link: "/about" },
@@ -9,16 +11,28 @@ const Nav = () => {
     { id: 4, name: "Blog", link: "/blog" },
   ];
   return (
-    <nav className="flex justify-between py-5 items-center">
+    <nav className="flex justify-between py-5 md:items-center">
       <a href="/" className="text-3xl">
         Logo
       </a>
 
-      <ul className="flex gap-5">
+      <ul
+        className={`md:flex gap-5 md:relative absolute duration-500 ease-in ${
+          open ? "top-16 md:top-0" : "top-[-120px] md:top-0"
+        }`}
+      >
         {routes.map((link) => (
           <Link key={link.id} link={link}></Link>
         ))}
       </ul>
+
+      {/* menu bar */}
+      <div
+        onClick={() => setOpen(!open)}
+        className="navbar md:hidden w-6 h-6 text-black-500"
+      >
+        {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+      </div>
     </nav>
   );
 };
